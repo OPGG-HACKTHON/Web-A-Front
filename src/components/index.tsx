34919214bafbecdaa1b1330465aa630d5952ee0e
HTMLhead <mrdoo309@gmail.com>
2021-07-17 18:44:10 +0900

    ✅ 빌드를 위한 함수, 컴포넌트 추가, 간단한 테스트예시 추가

---
+++ src/components/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NextPage } from "next";
+
+const IndexComponent: NextPage = () => {
+  return (
+    <>
+      <div>indexCompoennt</div>
+    </>
+  );
+};
+
+export default IndexComponent;
