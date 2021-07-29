318a118252bcd5627aab098d3792f22ec505d9b9
HTMLhead <mrdoo309@gmail.com>
2021-07-29 20:58:51 +0900

    :sparkles: reset과 webfont적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,13 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css"
+            rel="stylesheet"
+            type="text/css"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -1,9 +1,17 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "./reset";
 
 const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
   * {
-  box-sizing: border-box;
-}
+    box-sizing: border-box;
+  }
+
+  body {
+    font-size: 10px;
+    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+  }
 `;
 
 export default GlobalStyle;
