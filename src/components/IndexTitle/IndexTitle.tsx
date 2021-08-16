051f33ee9396ed62f36ff92eaa5240baab818b18
HTMLhead <mrdoo309@gmail.com>
2021-08-16 13:58:35 +0900

    :wrench: indexTitle컴포넌트의 스타일 코드 분리

--- src/components/IndexTitle/IndexTitle.tsx
+++ src/components/IndexTitle/IndexTitle.tsx
@@ -1,48 +1,14 @@
+// Type
 import { IIndexTitleProps } from "./IndexTitle.type";
-import styled from "styled-components";
-
-const Title = styled.div`
-  display: flex;
-  font-size: 2.8rem;
-  line-height: 3.5rem;
-`;
-
-const IndexContainer = styled.div`
-  display: flex;
-`;
-
-const SelectedIndex = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 3.5rem;
-  height: 3.5rem;
-  border-radius: 0.5rem;
-  border: 0.2rem solid ${({ theme }) => theme.palette.primary.main};
-  color: ${({ theme }) => theme.palette.primary.main};
-`;
-
-const Index = styled.div`
-  font-size: 1.8rem;
-  line-height: 2.3rem;
-`;
-
-const NonSelectedIndex = styled.div`
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 3.5rem;
-  height: 3.5rem;
-  color: ${({ theme }) => theme.palette.primary.main};
-`;
-
-const TitleContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 2.5rem;
-  border-bottom: 0.2rem solid ${({ theme }) => theme.palette.grayScale[500]};
-`;
+// Style
+import {
+  Title,
+  TitleContainer,
+  NonSelectedIndex,
+  SelectedIndex,
+  Index,
+  IndexContainer,
+} from "./IndexTitle.style";
 
 const IndexTitle: React.FC<IIndexTitleProps> = ({
   title,
@@ -60,11 +26,11 @@ const IndexTitle: React.FC<IIndexTitleProps> = ({
         {indexList.map((i: number) =>
           selectedIndex === i ? (
             <SelectedIndex key={i}>
-              <Index>{i}</Index>
+              <Index>{i + 1}</Index>
             </SelectedIndex>
           ) : (
             <NonSelectedIndex key={i} onClick={() => clickHandler(i)}>
-              <Index>{i}</Index>
+              <Index>{i + 1}</Index>
             </NonSelectedIndex>
           )
         )}
