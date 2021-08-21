629043c0ed0286c556c4e0f408750d2f1b459ec0
dev-jun <jun094@naver.com>
2021-08-21 17:56:27 +0900

    ✨about 페이지 생성

---
+++ src/pages/about/[id].tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NextPage } from "next";
+
+const AboutPage: NextPage = () => {
+  return <></>;
+};
+export async function getServerSideProps() {
+  return {};
+}
+export default AboutPage;
