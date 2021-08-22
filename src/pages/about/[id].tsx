d1afad3532d91b81051b56f301ec3775974861cd
dev-jun <jun094@naver.com>
2021-08-23 00:01:05 +0900

    ✨ Dynamic Routes, getInitialProps 생성

--- src/pages/about/[id].tsx
+++ src/pages/about/[id].tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { NextPage } from "next";
+import { NextPage, NextPageContext } from "next";
 
-const AboutPage: NextPage = () => {
-  return <></>;
+import Axios from "axios";
+
+export type AboutPageProps = {
+  item: {
+    id: number;
+  };
 };
-export async function getServerSideProps() {
-  return {};
+
+const AboutPage: NextPage<AboutPageProps> = ({ item }) => {
+  console.log(item);
+  return <>hi {item.id}</>;
+};
+
+interface Context extends NextPageContext {
+  // any modifications to the default context, e.g. query types
 }
+
+AboutPage.getInitialProps = async (ctx: Context) => {
+  const id = ctx.query.id;
+  const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await Axios.get(apiUrl);
+  const data = res.data;
+
+  return { item: data };
+};
+
 export default AboutPage;
