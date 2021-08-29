4b2ab57e30231443ccf84e720f9ac2d252b2b1ee
HTMLhead <mrdoo309@gmail.com>
2021-08-29 14:29:30 +0900

    :wrench: 타입 파일 이동, 변경된 아이콘 적용

--- src/components/PreviewCarousel/PreviewCarouselModal/PreviewCarouselModal.type.ts
+++
@@ -1,7 +0,0 @@
-export interface IPreviewCarouselModal {
-  selectedIndex: number;
-  thumbnailListState: Array<string>;
-  handleLeftClick: () => void;
-  handleRightClick: () => void;
-  handleModalClose: () => void;
-}
