ecbdc6311145b3b3ca417ff3c5d2833d7946924a
HTMLhead <mrdoo309@gmail.com>
2021-09-05 19:57:48 +0900

    :sparkles: 공용으로 사용되는 CarouselModal제작

---
+++ src/components/CarouselModal/CarouselModal.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Image from "next/image";
+
+import close from "assets/images/close.svg";
+
+import {
+  Dot,
+  PickedImg,
+  PickedVideo,
+  DotWrapper,
+  IndexWrapper,
+  ButtonWrapper,
+  CloseIconWrapper,
+  ModalWrapper,
+  ModalHeader,
+} from "./CarouselModal.style";
+
+import { ArrowBtn } from "components/ArrowBtn";
+
+export interface ICarouselModal {
+  selectedIndex: number;
+  itemList: Array<string>;
+  handlePrevClick: () => void;
+  handleNextClick: () => void;
+  handleModalClose: () => void;
+  video: boolean;
+}
+
+const CarouselModal: React.FC<ICarouselModal> = ({
+  selectedIndex,
+  itemList,
+  handlePrevClick,
+  handleNextClick,
+  handleModalClose,
+  video,
+}) => {
+  return (
+    <>
+      <ButtonWrapper style={{ left: "22rem" }}>
+        <ArrowBtn left white onClick={handlePrevClick} />
+      </ButtonWrapper>
+      <ModalWrapper>
+        <ModalHeader>
+          <IndexWrapper>
+            {selectedIndex} / {video ? itemList.length : itemList.length - 2}
+          </IndexWrapper>
+          <CloseIconWrapper onClick={handleModalClose}>
+            <Image unoptimized src={close} height={15} width={15} />
+          </CloseIconWrapper>
+        </ModalHeader>
+        {video ? (
+          <>
+            <PickedVideo controls>
+              <source src={itemList[selectedIndex]} type="video/mp4" />
+            </PickedVideo>
+            <DotWrapper>
+              {itemList.map((item, i) => (
+                <Dot key={`${item}-${i}`} selected={i === selectedIndex} />
+              ))}
+            </DotWrapper>
+          </>
+        ) : (
+          <PickedImg
+            style={{
+              backgroundImage: `url(${itemList[selectedIndex]})`,
+            }}
+          />
+        )}
+      </ModalWrapper>
+      <ButtonWrapper style={{ right: "22rem" }}>
+        <ArrowBtn white onClick={handleNextClick} />
+      </ButtonWrapper>
+    </>
+  );
+};
+
+export default CarouselModal;
