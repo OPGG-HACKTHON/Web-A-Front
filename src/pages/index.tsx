572b69672833a96627357a9661d0b29826af3166
HTMLhead <mrdoo309@gmail.com>
2021-09-07 15:39:59 +0900

    :wrench: 메인페이지에 이런 인디칩 어때요 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,36 @@
 import React from "react";
 import { NextPage } from "next";
 
-import Roulette from "components/Main/Roulette";
+import { pickType } from "components/Main/Carousel/Carousel";
 
-const IndexPage: NextPage = () => {
+import client from "lib/customAxios";
+
+import Roulette from "components/Main/Roulette";
+import Carousel from "components/Main/Carousel";
+
+export interface IndexPageProps {
+  recommendList: [pickType];
+}
+
+const IndexPage: NextPage<IndexPageProps> = ({ recommendList }) => {
   return (
     <>
       <Roulette />
+      <Carousel {...{ recommendList, onScreenCount: 4 }} />
     </>
   );
+};
+
+IndexPage.getInitialProps = async () => {
+  const apiUrl = `/api/home`;
+  const res = await client.get(apiUrl);
+  const {
+    data: {
+      data: { random_rec_list },
+    },
+  } = res;
+
+  return { recommendList: random_rec_list };
 };
 
 export default IndexPage;
