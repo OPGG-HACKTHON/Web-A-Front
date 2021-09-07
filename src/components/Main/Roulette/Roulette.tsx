ca9364ff6f783a560ab2e0ea762092d5e7796987
Sh031224 <1cktmdgh2@gmail.com>
2021-09-07 22:20:57 +0900

    :sparkles: Roulette 번역 적용

--- src/components/Main/Roulette/Roulette.tsx
+++ src/components/Main/Roulette/Roulette.tsx
@@ -1,5 +1,6 @@
 import debounce from "debounce";
 import { useState } from "react";
+import { useTranslation } from "next-i18next";
 
 import tooltip from "assets/images/roulette/tooltip.svg";
 
@@ -16,6 +17,8 @@ import RoluletteBubble from "./RoluletteBubble";
 import RoulettePlay from "./RoulettePlay";
 
 const Roulette: React.FC = () => {
+  const { t } = useTranslation("main");
+
   const [isTooltipShow, setIsTooltipShow] = useState<boolean>(false);
 
   const handleOnMouseEnter = debounce(() => setIsTooltipShow(true), 500);
@@ -27,23 +30,18 @@ const Roulette: React.FC = () => {
   return (
     <RouletteWrapper>
       <RouletteContent>
-        <RouletteTitle>인디칩 키워드를 골라보세요!</RouletteTitle>
+        <RouletteTitle>{t("main_roulette_title")}</RouletteTitle>
         <RouletteSubTitleWrapper>
-          <RouletteSubTitle>
-            키워드를 클릭해서 랜덤으로 추천 게임을 받아보세요!
-          </RouletteSubTitle>
+          <RouletteSubTitle>{t("main_roulette_subtitle")}</RouletteSubTitle>
           <RouletteSubTitleTooltip
-            unoptimized
             src={tooltip}
             onMouseEnter={handleOnMouseEnter}
           />
           <RoluletteBubble
             isShow={isTooltipShow}
             onClose={handleOnCloseTooltip}
-            title="랜덤 게임 추천기 안내"
-            content={
-              "잼픽의 인디칩은 인디게임을 의미합니다.\n\n하단의 키워드를 최대 6개까지 선택하여 랜덤\n으로 게임을 추천 받아보세요."
-            }
+            title={t("main_roulette_bubble_title")}
+            content={t("main_roulette_bubble_content")}
           />
         </RouletteSubTitleWrapper>
         <RouletteKeyword />
