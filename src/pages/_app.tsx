c5af990e16a921e8135553ab406c9fb85f171276
Sh031224 <1cktmdgh2@gmail.com>
2021-09-08 20:42:37 +0900

    :bug: this를 못찾는 오류 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,37 +3,28 @@ import { appWithTranslation } from "next-i18next";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 
-import { updateLocale } from "lib/customAxios";
-
+import LocaleProvider from "components/LocaleProvider";
 import Header from "components/Header";
 import PageWrapper from "components/PageWrapper";
 import GlobalStyle from "style/GlobalStyle";
 import theme from "style/theme";
 
 class MyApp extends App {
-  componentDidMount() {
-    updateLocale(this.props.router.locale);
-  }
-
-  componentDidUpdate(prevProps: typeof this.props) {
-    if (this.props.router.locale !== prevProps.router.locale) {
-      updateLocale(this.props.router.locale);
-    }
-  }
-
   render() {
     const { Component, pageProps } = this.props;
 
     return (
-      <ThemeProvider {...{ theme }}>
-        <RecoilRoot>
-          <Header />
-          <PageWrapper>
-            <Component {...pageProps} />
-          </PageWrapper>
-          <GlobalStyle />
-        </RecoilRoot>
-      </ThemeProvider>
+      <LocaleProvider>
+        <ThemeProvider {...{ theme }}>
+          <RecoilRoot>
+            <Header />
+            <PageWrapper>
+              <Component {...pageProps} />
+            </PageWrapper>
+            <GlobalStyle />
+          </RecoilRoot>
+        </ThemeProvider>
+      </LocaleProvider>
     );
   }
 }
