9e938005a1ddbac1ecd63311de786752a8287d56
HTMLhead <mrdoo309@gmail.com>
2021-09-13 22:01:49 +0900

    :sparkles: 이달의 출시 인디칩 캐로샐 목데이터 제작

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,19 @@ import client, { updateLocale } from "lib/customAxios";
 
 import Roulette from "components/Main/Roulette";
 import Carousel from "components/Main/Carousel";
+import ReleasedIndieChip from "components/Main/ReleasedIndieChip/ReleasedIndieChip";
 
 export interface IndexPageProps {
   recommendList: [pickType];
 }
 
 const IndexPage: NextPage<IndexPageProps> = ({ recommendList }) => {
+  const releasedIndieChips = recommendList.slice(4, 12);
   return (
     <>
       <Roulette />
       <Carousel {...{ recommendList, onScreenCount: 4 }} />
+      <ReleasedIndieChip {...{ releasedIndieChips }} />
     </>
   );
 };
