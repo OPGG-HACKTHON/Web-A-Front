1553f2163a7729eb1f92d7314cb402b765495f75
Sh031224 <1cktmdgh2@gmail.com>
2021-09-13 22:43:28 +0900

    :sparkles: 언어 변경 버튼 추가

--- src/components/Header/Header.style.ts
+++ src/components/Header/Header.style.ts
@@ -7,3 +7,32 @@ export const HeaderWrapper = styled.header`
   display: flex;
   justify-content: center;
 `;
+
+export const HeaderTranslationWrapper = styled.div`
+  position: absolute;
+  top: -0.5rem;
+  right: 100px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.6rem;
+  line-height: 28px;
+  letter-spacing: -0.03em;
+  color: ${({ theme }) => theme.palette.grayScale[300]};
+  width: 10rem;
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const HeaderTranslation = styled.button<{ isActive?: boolean }>`
+  color: ${({ theme, isActive }) =>
+    isActive ? theme.palette.grayScale[100] : theme.palette.grayScale[300]};
+  cursor: pointer;
+  border: none;
+  background-color: transparent;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.6rem;
+  line-height: 28px;
+  letter-spacing: -0.03em;
+  padding: 0;
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,22 @@ import Link from "next/link";
 
 import logo from "assets/images/logo.svg";
 
-import { HeaderWrapper } from "./Header.style";
+import {
+  HeaderTranslation,
+  HeaderTranslationWrapper,
+  HeaderWrapper,
+} from "./Header.style";
+import { useRouter } from "next/router";
 
 const Header: React.FC = () => {
+  const router = useRouter();
+
+  const handleChangeLanguage = (newLocale: string) => {
+    router.push(router.asPath, undefined, {
+      locale: newLocale,
+    });
+  };
+
   return (
     <HeaderWrapper>
       <Link href={"/"}>
@@ -14,6 +27,19 @@ const Header: React.FC = () => {
           <Image src={logo} height={27} />
         </a>
       </Link>
+      <HeaderTranslationWrapper>
+        <HeaderTranslation
+          isActive={router.locale === "ko"}
+          onClick={() => handleChangeLanguage("ko")}>
+          KOR
+        </HeaderTranslation>
+        <span>|</span>
+        <HeaderTranslation
+          isActive={router.locale === "en"}
+          onClick={() => handleChangeLanguage("en")}>
+          ENG
+        </HeaderTranslation>
+      </HeaderTranslationWrapper>
     </HeaderWrapper>
   );
 };
